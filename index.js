06c8e8c1eeedbca9d9dc980e7d660a5ebd0aa50e
Emily (Bare) Bernard <77028684+ebare012882@users.noreply.github.com>
2022-09-20 15:47:34 -0500

    did a lot in the js- added an array of images, created variables for tilesChosen, tilesChosenId, and gameboard, created a function for createGameBoard, created a function for flipTile, and made to do lists for CSS and JS for tomorrow

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
+// To do:
+    // 1. Finish checkForMatch function
+    // 2. 
+
 document.addEventListener('DOMContentLoaded', () => {
 
-const cardArr = [ 
+const imgArr = [ 
     {
         name: 'eiffelTower',
         img: '/frenchmemorygamepics/eiffeltower.png',
@@ -98,5 +102,48 @@ const cardArr = [
         img: '/frenchmemorygamepics/shakespeare.png',
     },
 ]
+
+const gameboard = document.querySelector('.gameboard')
+let tilesChosen = []
+let tilesChosenId = []
+
+function createGameBoard () {
+    for(let i = 0; i < imgArr.length; i++) {
+        let tile = document.createElement('img')
+        tile.setAttribute('src', 'url("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSpxLY-62UPoF1es3R8Ki6V5gYBLpnnJ1laig&usqp=CAU")');
+        tile.setAttribute('data-id', i)
+        // tile.addEventListener('click', flipcard)
+        gameboard.appendChild(tile)
+    }
+}
+
+
+// need to make a function to check for a match
+
+
+
+
+
+// need make a function to flip the tiles
+const flipTile() => {
+    let tileId = this.getAttribute('data-id')
+    tilesChosen.push(imgArr[tileId].name)
+    tilesChosenId.push(tileId)
+    this.setAttribute('src', imgArr[tileId].img)
+    if (tilesChosen.length === 2) {
+        setTimeout(checkForMatch, 500)
+    }
+}
+
+createGameBoard()
+
+
+
+
+
+
+
+
+
 
 })
