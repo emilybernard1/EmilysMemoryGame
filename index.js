9ab96ad12e75f3a3e3af1f08ff69ba794b5c2863
Emily (Bare) Bernard <77028684+ebare012882@users.noreply.github.com>
2022-09-20 15:13:07 -0500

    started the js with an array of my images and DOMContentLoaded and added a player scores div to html and css

---
+++ index.js
@@ -0,0 +1,102 @@
+document.addEventListener('DOMContentLoaded', () => {
+
+const cardArr = [ 
+    {
+        name: 'eiffelTower',
+        img: '/frenchmemorygamepics/eiffeltower.png',
+    }, 
+    {
+        name: 'eiffelTower',
+        img: '/frenchmemorygamepics/eiffeltower.png',
+    }, 
+    {
+        name: 'angelinas',
+        img: '/frenchmemorygamepics/angelinas.png',
+    },
+    {
+        name: 'angelinas',
+        img: '/frenchmemorygamepics/angelinas.png',
+    },
+    {
+        name: 'arcDeTriomphe',
+        img: '/frenchmemorygamepics/arcdetriomphe.png',
+    },
+    {
+        name: 'arcDeTriomphe',
+        img: '/frenchmemorygamepics/arcdetriomphe.png',
+    },
+    {
+        name: 'head',
+        img: '/frenchmemorygamepics/head.png'
+    },
+    {
+        name: 'head',
+        img: '/frenchmemorygamepics/head.png'
+    },
+    {
+        name: 'hotelDeVille',
+        img: '/frenchmemorygamepics/hoteldeville.png',
+    },
+    {
+        name: 'hotelDeVille',
+        img: '/frenchmemorygamepics/hoteldeville.png',
+    },
+    {
+        name: 'laduree',
+        img: '/frenchmemorygamepics/laduree.png'
+    },
+    {
+        name: 'laduree',
+        img: '/frenchmemorygamepics/laduree.png'
+    },
+    {
+        name: 'louvre',
+        img: '/frenchmemorygamepics/louvre.png',
+    },
+    {
+        name: 'louvre',
+        img: '/frenchmemorygamepics/louvre.png',
+    },
+    {
+        name: 'metroCite',
+        img: '/frenchmemorygamepics/metrocite.png',
+    },
+    {
+        name: 'metroCite',
+        img: '/frenchmemorygamepics/metrocite.png',
+    },
+    {
+        name: 'montMartre',
+        img: '/frenchmemorygamepics/montmartre.png',
+    },
+    {
+        name: 'montMartre',
+        img: '/frenchmemorygamepics/montmartre.png',
+    },
+    {
+        name: 'notreDame',
+        img: '/frenchmemorygamepics/notredame.png',
+    },
+    {
+        name: 'notreDame',
+        img: '/frenchmemorygamepics/notredame.png',
+    },
+    {
+        name: 'sacreCoeur',
+        img: '/frenchmemorygamepics/sacrecoeur.png',
+    },
+    {
+        name: 'sacreCoeur',
+        img: '/frenchmemorygamepics/sacrecoeur.png',
+    },
+    {
+        name: 'shakespeare',
+        img: '/frenchmemorygamepics/shakespeare.png',
+    },
+    {
+        name: 'shakespeare',
+        img: '/frenchmemorygamepics/shakespeare.png',
+    },
+]
+
+})
