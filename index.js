111e0b5a0870e47bfdf46c4a406d210d4e3551b0
Emily (Bare) Bernard <77028684+ebare012882@users.noreply.github.com>
2022-09-22 14:51:25 -0500

    got the gameboard to populate correctly and only lets the player click two tiles and alerts work correctly now

--- index.js
+++ index.js
@@ -80,8 +80,8 @@ const imgArr = [
     },
     {
         name: 'notreDame',
-        img: '/renchmemorygamepics/notredame.png'
-    }
+        img: 'frenchmemorygamepics/notredame.png'
+    },
     {
         name: 'notreDame',
         img: 'frenchmemorygamepics/notredame.png'
@@ -108,6 +108,7 @@ imgArr.sort(() => 0.5 - Math.random())
 
 const gameboard = document.querySelector('.gameboard')
 const scoreDisplay = document.querySelector('#result')
+const refaireButton = document.querySelector('.refaireButton')
 let tilesChosen = []
 let tilesChosenId = []
 const tilesWon = []
@@ -116,8 +117,9 @@ const tilesWon = []
 function createGameBoard () {
     for(let i = 0; i < imgArr.length; i++) {
         let tile = document.createElement('img')
-        tile.setAttribute('src', 'frenchmemorygamepics/poppy.png');
-        tile.setAttribute('data-id', i)
+        tile.setAttribute('src', 'frenchmemorygamepics/poppyfield.png');
+        tile.setAttribute('id', `tile-${i}`)
+        tile.setAttribute('class', 'carreau')
         tile.addEventListener('click', flipTile)
         gameboard.appendChild(tile)
     }
@@ -125,24 +127,49 @@ function createGameBoard () {
 
 createGameBoard()
 
+function flipTile(event){
+    let tileId = event.srcElement.id
+    let tileIndex = parseInt(tileId.split('-')[1])
+    console.log(event, event.srcElement.id, tileIndex)
+    // let clickedTile = document.querySelector(`#${event.srcElement.id}`)
+    tilesChosen.push(imgArr[tileIndex].name)
+    tilesChosenId.push(tileId)
+    this.setAttribute('src', imgArr[tileIndex].img)
+    if (tilesChosen.length === 2) {
+        setTimeout(checkForMatch, 500)
+    }
+}
+
+
+
+
 // need to make a function to check for a match
 function checkForMatch(){
-    let tiles = document.querySelectorAll('img')
+    console.log(tilesChosen, tilesChosenId)
+    // let tiles = document.querySelectorAll('img')
+    // console.log(tiles)
+    if(tilesChosenId.length !== 2){
+        return
+    }
     const optionOneId = tilesChosenId[0]
     const optionTwoId = tilesChosenId[1]
+    let pickedTile1 = document.querySelector(`#${optionOneId}`)
+    let pickedTile2 = document.querySelector(`#${optionTwoId}`)
     if (tilesChosen[0] === tilesChosen[1]){
         alert('Youpi! Tu as fait un match! Yay! You made a match!')
-        tiles[optionOneId].setAttribute('src', 'frenchmemorygamespics/white.png')
-        tiles[optionTwoId].setAttribute('src', 'frenchmemorygamespics/white.png')
+        pickedTile1.setAttribute('src', 'frenchmemorygamespics/white.png')
+        pickedTile2.setAttribute('src', 'frenchmemorygamespics/white.png')
         tilesWon.push(tilesChosen)
     } else {
-        tiles[optionOneId].setAttribute('src', 'frenchmemorygamepics/poppy.png')
-        tiles.optionTwoId.setAttribute('src', 'frenchmemorygamepics/poppy.png')
+        pickedTile1.setAttribute('src', 'frenchmemorygamepics/poppyfield.png')
+        pickedTile2.setAttribute('src', 'frenchmemorygamepics/poppyfield.png')
         alert('Tant pis! Pas de match! Too bad! No match!')
     }
+    console.log('before erase', tilesChosen)
     tilesChosen = []
     tilesChosenId = []
     scoreDisplay.textContent = tilesWon.length
+    console.log('after erase', tilesChosen)
     if (tilesWon.length === imgArr.length/2) {
         scoreDisplay.textContent = 'Félicitations! Vous avez gagné! Congratulations! You won!'
     }
@@ -150,32 +177,23 @@ function checkForMatch(){
 
 checkForMatch()
 
-function flipTile(){
-    let tileId = this.getAttribute('data-id')
-    tilesChosen.push(imgArr[tileId].name)
-    tilesChosenId.push(tileId)
-    this.setAttribute('src', imgArr[tileId].img)
-    if (tilesChosen.length === 2) {
-        setTimeout(checkForMatch, 500)
-    }
-}
-
-flipTile()
-
-// Can I change the flipTile function to loook this it does below?
-// const flipTile () => {
-//     let tileId = this.getAttribute('data-id')
-//     tilesChosen.push(imgArr[tileId].name)
-//     tilesChosenId.push(tileId)
-//     this.setAttribute('src', imgArr[tileId].img)
-//     if (tilesChosen.length === 2) {
-//         setTimeout(checkForMatch, 500)
-//     }
-// }
-
+// when game is won-everything needs to hide
+// reset gameboard needs to remove the hidden and reset the poppy field pic
 
 // I need to modify the below (from Tic Tac Toe) to get it to work with this memory game.
-// const resetGameBoard = () => {
+const resetGameBoard = () => {
+    let tiles = document.querySelectorAll('.carreau')
+    console.log(tiles, 'in refaireButton')
+    tiles.forEach()
+
+}
+
+refaireButton.addEventListener('click', resetGameBoard);
+
+
+
+
+
 //     gameBoard = [];
 //     isGameActive = true;
 //     announcer.classList.add('hide');
@@ -200,7 +218,6 @@ flipTile()
 //     // })
 // }
 
-// resetButton.addEventListener('click', resetGameBoard);
 
 
 // Can I modify the below to work for this?
