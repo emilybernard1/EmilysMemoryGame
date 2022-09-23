4948052835cbf72471e91275593f488c50d21a25
Emily (Bare) Bernard <77028684+ebare012882@users.noreply.github.com>
2022-09-23 12:12:02 -0500

    improved the layout and functionality. Still stuck on getting the tiles to not be clickable once matched

--- index.js
+++ index.js
@@ -1,105 +1,58 @@
 // To Do:
 // 1. get the scores to show up
+// 2. Make it so they can't click on the "hidden" tiles once they are matched!! (Why are they resizing smaller and taking up different spaces on the board once matched??)
 
 document.addEventListener('DOMContentLoaded', () => {
 
 const imgArr = [ 
-    {
-        name: 'eiffelTower',
-        img: 'frenchmemorygamepics/eiffeltower.png'
-    }, 
-    {
-        name: 'eiffelTower',
-        img: 'frenchmemorygamepics/eiffeltower.png'
-    }, 
-    {
-        name: 'angelinas',
-        img: 'frenchmemorygamepics/angelinas.png'
-    },
-    {
-        name: 'angelinas',
-        img: 'frenchmemorygamepics/angelinas.png'
-    },
-    {
-        name: 'arcDeTriomphe',
-        img: 'frenchmemorygamepics/arcdetriomphe.png'
-    },
-    {
-        name: 'arcDeTriomphe',
-        img: 'frenchmemorygamepics/arcdetriomphe.png'
-    },
-    {
-        name: 'head',
-        img: 'frenchmemorygamepics/head.png'
-    },
-    {
-        name: 'head',
-        img: 'frenchmemorygamepics/head.png'
-    },
-    {
-        name: 'hotelDeVille',
-        img: 'frenchmemorygamepics/hoteldeville.png'
-    },
-    {
-        name: 'hotelDeVille',
-        img: 'frenchmemorygamepics/hoteldeville.png'
-    },
-    {
-        name: 'laduree',
-        img: 'frenchmemorygamepics/laduree.png'
-    },
-    {
-        name: 'laduree',
-        img: 'frenchmemorygamepics/laduree.png'
-    },
-    {
-        name: 'louvre',
-        img: 'frenchmemorygamepics/louvre.png'
-    },
-    {
-        name: 'louvre',
-        img: 'frenchmemorygamepics/louvre.png'
-    },
-    {
-        name: 'metroCite',
-        img: 'frenchmemorygamepics/metrocite.png'
-    },
-    {
-        name: 'metroCite',
-        img: 'frenchmemorygamepics/metrocite.png'
-    },
-    {
-        name: 'montMartre',
-        img: 'frenchmemorygamepics/montmartre.png'
-    },
-    {
-        name: 'montMartre',
-        img: 'frenchmemorygamepics/montmartre.png'
-    },
-    {
-        name: 'notreDame',
-        img: 'frenchmemorygamepics/notredame.png'
-    },
-    {
-        name: 'notreDame',
-        img: 'frenchmemorygamepics/notredame.png'
-    },
-    {
-        name: 'sacreCoeur',
-        img: 'frenchmemorygamepics/sacrecoeur.png'
-    },
-    {
-        name: 'sacreCoeur',
-        img: 'frenchmemorygamepics/sacrecoeur.png'
-    },
-    {
-        name: 'shakespeare',
-        img: 'frenchmemorygamepics/shakespeare.png'
-    },
-    {
-        name: 'shakespeare',
-        img: 'frenchmemorygamepics/shakespeare.png'
-    },
+    { name: 'eiffelTower',
+        img: 'frenchmemorygamepics/eiffeltower.png' }, 
+    { name: 'eiffelTower',
+        img: 'frenchmemorygamepics/eiffeltower.png' }, 
+    { name: 'angelinas',
+        img: 'frenchmemorygamepics/angelinas.png' },
+    { name: 'angelinas',
+        img: 'frenchmemorygamepics/angelinas.png' },
+    { name: 'arcDeTriomphe',
+        img: 'frenchmemorygamepics/arcdetriomphe.png' },
+    { name: 'arcDeTriomphe',
+        img: 'frenchmemorygamepics/arcdetriomphe.png' },
+    { name: 'head',
+        img: 'frenchmemorygamepics/head.png' },
+    { name: 'head',
+        img: 'frenchmemorygamepics/head.png' },
+    { name: 'hotelDeVille',
+        img: 'frenchmemorygamepics/hoteldeville.png' },
+    { name: 'hotelDeVille',
+        img: 'frenchmemorygamepics/hoteldeville.png' },
+    { name: 'laduree',
+        img: 'frenchmemorygamepics/laduree.png' },
+    { name: 'laduree',
+        img: 'frenchmemorygamepics/laduree.png' },
+    { name: 'louvre',
+        img: 'frenchmemorygamepics/louvre.png' },
+    { name: 'louvre',
+        img: 'frenchmemorygamepics/louvre.png' },
+    { name: 'metroCite',
+        img: 'frenchmemorygamepics/metrocite.png' },
+    { name: 'metroCite',
+        img: 'frenchmemorygamepics/metrocite.png' },
+    { name: 'montMartre',
+        img: 'frenchmemorygamepics/montmartre.png' },
+    { name: 'montMartre',
+        img: 'frenchmemorygamepics/montmartre.png' },
+    { name: 'notreDame',
+        img: 'frenchmemorygamepics/notredame.png' },
+    { name: 'notreDame',
+        img: 'frenchmemorygamepics/notredame.png' },
+    { name: 'sacreCoeur',
+        img: 'frenchmemorygamepics/sacrecoeur.png' },
+    { name: 'sacreCoeur',
+        img: 'frenchmemorygamepics/sacrecoeur.png' },
+    { name: 'shakespeare',
+        img: 'frenchmemorygamepics/shakespeare.png' },
+    { name: 'shakespeare',
+        img: 'frenchmemorygamepics/shakespeare.png' },
 ]
 
 imgArr.sort(() => 0.5 - Math.random())
@@ -113,7 +66,8 @@ const tilesWon = []
 
 let isPlayerTurn1 = true
 
-// need function to create the gameboard
+
+// function to create the gameboard
 function createGameBoard () {
     for(let i = 0; i < imgArr.length; i++) {
         let tile = document.createElement('img')
@@ -131,6 +85,8 @@ function createGameBoard () {
 
 createGameBoard()
 
+
+// function for flipping the tiles
 function flipTile(event){
     let tileId = event.srcElement.id
     let tileIndex = parseInt(tileId.split('-')[1])
@@ -145,9 +101,9 @@ function flipTile(event){
 }
 
 
-// need to make a function to check for a match
+// function for checking a match
 function checkForMatch(){
-    // next steps! lets add a check of the current players turn (who's turn is it) on each successful match, get the appropriate players score fromt the DOM, and change it by incrementing
+    // next steps! lets add a check of the current players turn (who's turn is it) on each successful match, get the appropriate players score fromt the DOM, and change it by incrementing (i++)
     // see MDN HTML element.innerText and getElementById
     console.log(tilesChosen, tilesChosenId)
     // let tiles = document.querySelectorAll('img')
@@ -160,10 +116,13 @@ function checkForMatch(){
     let pickedTile1 = document.querySelector(`#${optionOneId}`)
     let pickedTile2 = document.querySelector(`#${optionTwoId}`)
     if (tilesChosen[0] === tilesChosen[1]){
+        // can I make a function within the alert that names the monument?
         alert('Youpi! Tu as fait un match! Yay! You made a match!')
         pickedTile1.setAttribute('src', 'frenchmemorygamespics/white.png')
         pickedTile2.setAttribute('src', 'frenchmemorygamespics/white.png')
         tilesWon.push(tilesChosen)
+        // tilesWon.classList.add('tiles-won')
+        // pickedTile2.classList.add('tiles-won')
     } else {
         pickedTile1.setAttribute('src', 'frenchmemorygamepics/poppyfield.png')
         pickedTile2.setAttribute('src', 'frenchmemorygamepics/poppyfield.png')
@@ -177,16 +136,12 @@ function checkForMatch(){
     if (tilesWon.length === imgArr.length/2) {
         scoreDisplay.textContent = 'Félicitations! Vous avez gagné! Congratulations! You won!'
     }
-    isPlayerTurn1 = !isPlayerTurn1
+    
 }
 
 checkForMatch()
 
 
-// when game is won-everything needs to hide
-// reset gameboard needs to remove the hidden and reset the poppy field pic
-
-// I need to modify the below (from Tic Tac Toe) to get it to work with this memory game.
 const resetGameBoard = () => {
     let tiles = document.querySelectorAll('.carreau')
     console.log(tiles, 'in refaireButton')
@@ -197,6 +152,69 @@ const resetGameBoard = () => {
 }
 
 refaireButton.addEventListener('click', resetGameBoard);
+
+
+
+// tilesWon.style.pointerEvents = "none";
+
+
+// trying to lock the board so other cards can't be clicked once a match is made. 
+// can I call them cards here even though they were called tiles above?
+
+// const cards = document.querySelectorAll('.memory-card');
+
+//     let hasFlippedCard = false;
+//     let lockBoard = false;
+//     let firstCard, secondCard;
+
+// function flipCard() {
+//     if (lockBoard) return;
+//     if (this === firstCard) return;
+//     this.classList.add('flip');
+//     if (!hasFlippedCard) {
+//       hasFlippedCard = true;
+//       firstCard = this;
+//       return;
+//     }
+//     secondCard = this;
+
+//     checkForMatch();
+//   }
+//   function checkForMatch() {
+//     let isMatch = firstCard.dataset.framework === secondCard.dataset.framework;
+//     isMatch ? disableCards() : unflipCards();
+//   }
+//   function disableCards() {
+//     firstCard.removeEventListener('click', flipCard);
+//     secondCard.removeEventListener('click', flipCard);
+//     resetBoard();
+//   }
+//   function unflipCards() {
+//      lockBoard = true;
+
+//     setTimeout(() => {
+//       firstCard.classList.remove('flip');
+//       secondCard.classList.remove('flip');
+
+//      resetBoard();
+//     }, 1500);
+//   }
+// function resetBoard() {
+//     [hasFlippedCard, lockBoard] = [false, false];
+//     [firstCard, secondCard] = [null, null];
+//   }
+// (function shuffle() {
+//     cards.forEach(card => {
+//       let ramdomPos = Math.floor(Math.random() * 12);
+//       card.style.order = ramdomPos;
+//     });
+//   })();
+//   cards.forEach(card => card.addEventListener('click', flipCard));
+
+
+
+
+
 
 
 
