b54db1ab9023aa526ea68e29ce8161fc2b544c9f
Emily (Bare) Bernard <77028684+ebare012882@users.noreply.github.com>
2022-09-26 15:41:53 -0500

    broke the randomize images function in the last commit so I fized it in this one

--- index.js
+++ index.js
@@ -1,33 +1,33 @@
 document.addEventListener('DOMContentLoaded', () => {
 
-    const imgArr = [ 
-        { name: 'eiffelTower', img: 'frenchmemorygamepics/eiffeltower.png' }, 
-        { name: 'eiffelTower', img: 'frenchmemorygamepics/eiffeltower.png' }, 
-        { name: 'angelinas', img: 'frenchmemorygamepics/angelinas.png' },
-        { name: 'angelinas', img: 'frenchmemorygamepics/angelinas.png' },
-        { name: 'arcDeTriomphe', img: 'frenchmemorygamepics/arcdetriomphe.png' },
-        { name: 'arcDeTriomphe', img: 'frenchmemorygamepics/arcdetriomphe.png' },
-        { name: 'head', img: 'frenchmemorygamepics/head.png' },
-        { name: 'head', img: 'frenchmemorygamepics/head.png' },
-        { name: 'hotelDeVille', img: 'frenchmemorygamepics/hoteldeville.png' },
-        { name: 'hotelDeVille', img: 'frenchmemorygamepics/hoteldeville.png' },
-        { name: 'laduree', img: 'frenchmemorygamepics/laduree.png' },
-        { name: 'laduree', img: 'frenchmemorygamepics/laduree.png' },
-        { name: 'louvre', img: 'frenchmemorygamepics/louvre.png' },
-        { name: 'louvre', img: 'frenchmemorygamepics/louvre.png' },
-        { name: 'metroCite', img: 'frenchmemorygamepics/metrocite.png' },
-        { name: 'metroCite', img: 'frenchmemorygamepics/metrocite.png' },
-        { name: 'montMartre', img: 'frenchmemorygamepics/montmartre.png' },
-        { name: 'montMartre', img: 'frenchmemorygamepics/montmartre.png' },
-        { name: 'notreDame', img: 'frenchmemorygamepics/notredame.png' },
-        { name: 'notreDame', img: 'frenchmemorygamepics/notredame.png' },
-        { name: 'sacreCoeur', img: 'frenchmemorygamepics/sacrecoeur.png' },
-        { name: 'sacreCoeur', img: 'frenchmemorygamepics/sacrecoeur.png' },
-        { name: 'shakespeare', img: 'frenchmemorygamepics/shakespeare.png' },
-        { name: 'shakespeare', img: 'frenchmemorygamepics/shakespeare.png' },
-    ]
+const imgArr = [ 
+    { name: 'eiffelTower', img: 'frenchmemorygamepics/eiffeltower.png' }, 
+    { name: 'eiffelTower', img: 'frenchmemorygamepics/eiffeltower.png' }, 
+    { name: 'angelinas', img: 'frenchmemorygamepics/angelinas.png' },
+    { name: 'angelinas', img: 'frenchmemorygamepics/angelinas.png' },
+    { name: 'arcDeTriomphe', img: 'frenchmemorygamepics/arcdetriomphe.png' },
+    { name: 'arcDeTriomphe', img: 'frenchmemorygamepics/arcdetriomphe.png' },
+    { name: 'head', img: 'frenchmemorygamepics/head.png' },
+    { name: 'head', img: 'frenchmemorygamepics/head.png' },
+    { name: 'hotelDeVille', img: 'frenchmemorygamepics/hoteldeville.png' },
+    { name: 'hotelDeVille', img: 'frenchmemorygamepics/hoteldeville.png' },
+    { name: 'laduree', img: 'frenchmemorygamepics/laduree.png' },
+    { name: 'laduree', img: 'frenchmemorygamepics/laduree.png' },
+    { name: 'louvre', img: 'frenchmemorygamepics/louvre.png' },
+    { name: 'louvre', img: 'frenchmemorygamepics/louvre.png' },
+    { name: 'metroCite', img: 'frenchmemorygamepics/metrocite.png' },
+    { name: 'metroCite', img: 'frenchmemorygamepics/metrocite.png' },
+    { name: 'montMartre', img: 'frenchmemorygamepics/montmartre.png' },
+    { name: 'montMartre', img: 'frenchmemorygamepics/montmartre.png' },
+    { name: 'notreDame', img: 'frenchmemorygamepics/notredame.png' },
+    { name: 'notreDame', img: 'frenchmemorygamepics/notredame.png' },
+    { name: 'sacreCoeur', img: 'frenchmemorygamepics/sacrecoeur.png' },
+    { name: 'sacreCoeur', img: 'frenchmemorygamepics/sacrecoeur.png' },
+    { name: 'shakespeare', img: 'frenchmemorygamepics/shakespeare.png' },
+    { name: 'shakespeare', img: 'frenchmemorygamepics/shakespeare.png' },
+]
 
-// imgArr.sort(() => 0.5 - Math.random())
+imgArr.sort(() => 0.5 - Math.random())
 
 const gameboard = document.querySelector('.gameboard')
 // const scoreDisplay = document.querySelector('#result')
